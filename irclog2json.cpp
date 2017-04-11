1f147d084a5904b6fe04e24e431a3e07c234efaa
ocha <ochaochaocha3@gmail.com>
2017-04-12 00:23:51 +0900

    オプションを指定できるようにする

--- irclog2json.cpp
+++ irclog2json.cpp
@@ -1,3 +1,4 @@
+#include "options.h"
 #include "converter.h"
 
 #include <string>
@@ -7,21 +8,65 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <clocale>
 #include <libgen.h>
+#include <getopt.h>
 
-void PrintUsage(char* argv0);
+/**
+ * @brief コマンドライン引数からオプションを解析する。
+ * @param [in] argc コマンドライン引数の個数。
+ * @param [in] argv コマンドライン引数の配列。
+ * @param [out] options オプションの設定先。
+ * @param [out] print_usage 使用法を出力するか。
+ * @retval <true> 有効なオプションのみ含まれたいた場合。
+ * @retval <false> 無効なオプションが含まれていた場合。
+ */
+static bool ParseOptions(
+  int argc, char* argv[], irclog2json::Options& options, bool& print_usage
+);
+/**
+ * @brief 使用法を表示する。
+ * @param argv0 コマンドライン引数の最初に設定されるプログラム名。
+ * @param os 出力ストリーム。
+ */;
+static void PrintUsage(char* argv0, std::ostream& os);
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    PrintUsage(argv[0]);
+  char* argv0 = argv[0];
+
+  std::setlocale(LC_ALL, "");
+
+  irclog2json::Options options;
+  bool print_usage;
+  bool no_invalid_option = ParseOptions(argc, argv, options, print_usage);
+
+  argc -= optind;
+  argv += optind;
+
+  if (print_usage) {
+    PrintUsage(argv0, no_invalid_option ? std::cout : std::cerr);
+    std::exit(no_invalid_option ? 0 : 1);
+  }
+
+  if (argc != 2) {
+    PrintUsage(argv0, std::cerr);
     std::exit(1);
   }
 
-  std::string filename(argv[1]);
-  std::string channel(argv[2]);
+  if (options.log_format == irclog2json::Options::LogFormat::NotSpecified) {
+    std::cerr << "Specify the log format." << std::endl;
+    std::exit(1);
+  }
 
-  char* file_basename = basename(argv[1]);
+  // basename(3) が文字列を変更するので退避する意味も含む
+  std::string filename(argv[0]);
 
+  std::string channel(argv[1]);
+
+  // ここで argv[0] は変更される
+  char* file_basename = basename(argv[0]);
+
+  // ファイル名から日付を抽出する
   struct tm tm_date;
   char* rest = strptime(file_basename, "%Y%m%d.txt", &tm_date);
   if (rest == NULL || *rest != '\0') {
@@ -29,14 +74,16 @@ int main(int argc, char* argv[]) {
     std::exit(1);
   }
 
+  // 出力ファイル名を決める
   std::string out_filename(filename);
+  // 末尾の .txt -> .json
   out_filename.replace(out_filename.size() - 3, 3, "json");
 
   std::ifstream ifs;
 
-  ifs.open(argv[1]);
+  ifs.open(filename);
   if (ifs.fail()) {
-    std::perror(argv[1]);
+    std::perror(filename.c_str());
     std::exit(1);
   }
 
@@ -49,11 +96,11 @@ int main(int argc, char* argv[]) {
 
   ofs.open(out_filename);
   if (ofs.fail()) {
-    std::perror(argv[1]);
+    std::perror(out_filename.c_str());
     std::exit(1);
   }
 
-  ofs << result.serialize(true);
+  ofs << result.serialize(options.pretty);
 
   ofs.close();
 
@@ -62,6 +109,57 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-void PrintUsage(char* argv0) {
-  std::cerr << "Usage: " << argv0 << " TIARRA_LOG_FILE CHANNEL" << std::endl;
+static bool ParseOptions(
+  int argc, char* argv[], irclog2json::Options& options, bool& print_usage
+) {
+  constexpr struct option long_options[] = {
+    {"tiarra", no_argument, nullptr, 't'},
+    {"pretty", no_argument, nullptr, 'p'},
+    {"help", no_argument, nullptr, 'h'},
+    {0, 0, 0, 0}
+  };
+  const char* optstring = "tph";
+
+  print_usage = false;
+
+  bool no_invalid_option = true;
+  int c;
+  int option_index = 0;
+  while (true) {
+    c = getopt_long(argc, argv, optstring, long_options, &option_index);
+    if (c == -1) {
+      break;
+    }
+
+    switch (c) {
+    case 't':
+      // -t, --tiarra
+      options.log_format = irclog2json::Options::LogFormat::Tiarra;
+      break;
+    case 'p':
+      // -p, --pretty
+      options.pretty = true;
+      break;
+    case 'h':
+      // -h, --help
+      print_usage = true;
+      break;
+    case '?':
+      // 無効なオプション
+      no_invalid_option = false;
+      print_usage = true;
+      break;
+    default:
+      break;
+    }
+  }
+
+  return no_invalid_option;
+}
+
+static void PrintUsage(char* argv0, std::ostream& os) {
+  os << "Usage: " << argv0 << " -t [-p] LOG_FILE CHANNEL" << std::endl;
+  os << "  -t, --tiarra      specify that the log file is Tiarra format" << std::endl;
+  os << "  -p, --pretty      output pretty-printed JSON" << std::endl;
+  os << "  -h, --help        display this help and exit" << std::endl;
 }
---
+++ options.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace irclog2json {
+  /** @brief オプション設定を示す構造体。 */
+  struct Options {
+  public:
+    /** @brief ログの書式を示す列挙型。 */
+    enum class LogFormat {
+      /** @brief 未指定。 */
+      NotSpecified,
+      /** @brief Tiarraが出力したログ。 */
+      Tiarra
+    };
+
+    /** @brief ログの書式。 */
+    LogFormat log_format;
+    /** @brief 見やすく出力するか。 */
+    bool pretty;
+
+    /** @brief オプション設定を初期化する。 */
+    Options() :
+      log_format(LogFormat::NotSpecified),
+      pretty(false)
+    {
+    }
+
+    ~Options() = default;
+
+    Options(Options const&) = default;
+    Options(Options&&) = default;
+    Options& operator =(Options const&) = default;
+    Options& operator =(Options&&) = default;
+  };
+}
