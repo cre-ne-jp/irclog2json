f245b5518dd40cf3e39ed447a6eb54ad65d8e4b0
ocha <ochaochaocha3@gmail.com>
2017-08-14 15:18:47 +0900

    UnicodeStringのまま置換した文字の復元を行うようにする

--- message/iso_2022_jp_line_converter.cpp
+++ message/iso_2022_jp_line_converter.cpp
@@ -4,18 +4,10 @@
 
 #include <vector>
 #include <string>
-#include <codecvt>
 #include <memory>
 #include <unicode/unistr.h>
 
 namespace {
-  /**
-   * @brief 文字コードをISO-2022-JPからUTF-8に変換する。
-   * @param s 変換する文字列。
-   * @return 変換後の文字列。
-   */
-  std::string Iso2022JpToUtf8(std::string const& s);
-
   /**
    * @brief mIRCの書式設定解除コードをエスケープする。
    * @param [in] s 変換する文字列。
@@ -37,21 +29,10 @@ namespace {
    * @brief mIRCの書式設定解除のエスケープを元に戻す。
    * @param s 変換する文字列。
    * @param plain_code_indices "^O" が何文字目にあったかを記録した配列。
-   * @return 変換後の文字列。
    *
    * 文字の場所の情報を参考にして "^Z" にエスケープされた "^O" を元に戻す。
    */
-  std::string UnescapeMIrcPlain(std::string const& s, std::vector<size_t> const& plain_code_indices);
-
-  std::string Iso2022JpToUtf8(std::string const& s) {
-    icu::UnicodeString src(s.c_str(), "iso-2022-jp");
-    size_t length = src.extract(0, src.length(), nullptr, "utf8");
-
-    std::vector<char> result(length + 1);
-    src.extract(0, src.length(), &result[0], "utf8");
-
-    return std::string(result.begin(), result.end() - 1);
-  }
+  void UnescapeMIrcPlain(icu::UnicodeString& s, std::vector<size_t> const& plain_code_indices);
 
   enum class Iso2022JpState {
     Ascii,
@@ -125,17 +106,12 @@ namespace {
     return result;
   }
 
-  std::string UnescapeMIrcPlain(std::string const& s, std::vector<size_t> const& plain_code_indices) {
-    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
-    std::u32string u32s = converter.from_bytes(s);
-
+  void UnescapeMIrcPlain(icu::UnicodeString& s, std::vector<size_t> const& plain_code_indices) {
     for (auto i : plain_code_indices) {
-      if (u32s[i] == U'\u001A') {
-        u32s[i] = U'\u000F';
+      if (s[i] == u'\u001A') {
+        s.replace(i, 1, u'\u000F');
       }
     }
-
-    return converter.to_bytes(u32s);
   }
 }
 
@@ -155,8 +131,11 @@ namespace irclog2json {
       std::vector<size_t> plain_code_indices;
 
       std::string escaped = EscapeMIrcPlain(line, plain_code_indices);
-      std::string line_utf8 = Iso2022JpToUtf8(escaped);
-      std::string unescaped = UnescapeMIrcPlain(line_utf8, plain_code_indices);
+      icu::UnicodeString escaped_unicode(escaped.c_str(), "iso-2022-jp");
+      UnescapeMIrcPlain(escaped_unicode, plain_code_indices);
+
+      std::string unescaped;
+      escaped_unicode.toUTF8String(unescaped);
 
       return line_converter_.ToMessage(unescaped);
     }
